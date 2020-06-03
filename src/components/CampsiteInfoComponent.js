f136619b8170c16007d5a2ad52bc5bfb49f1128b
Imran <imran_khan2888@hotmail.com>
2020-06-02 19:22:37 -0500

    Week3-Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    
-
-
-    renderCampsite(campsite) {
+    function RenderCampsite({campsite}) {
         return(
             <div className="col-md-5 m-1">
                 <Card>
@@ -16,12 +12,12 @@ class CampsiteInfo extends Component {
                     </CardBody>
                 </Card>
             </div>
-        )
+        );
 
     }
 
 
-    renderComments(comments){
+    function RenderComments({comments}){
         if(comments){
             return(
                 <div className="col-md-5 m-1">
@@ -34,31 +30,32 @@ class CampsiteInfo extends Component {
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
      
                     </div>
-                )
+                );
                 })}
                 </div>
-            )
+            );
         }
+        return <div />;
     }
 
     
 
 
     
-    render() {
-        if(this.props.campsite){
-            return(
+    function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
                     </div>
                 </div>
-            )
+            );
         }
-        return <div />
+        return <div />;
     }
-}
+
 
 
 
